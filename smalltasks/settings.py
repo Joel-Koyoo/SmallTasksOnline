86d559fd4a10dc835dca05a4bad4d3650c31523a
Joel <nyambok97@gmail.com>
2022-07-07 14:26:44 +0300

    Debug

--- smalltasks/settings.py
+++ smalltasks/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from hashlib import md5
 from pathlib import Path
 import os
+from pickle import FALSE
+from sre_parse import FLAGS
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u=)jnx53m#lzn3v0guapit8iu!(uf-*r4yrby0szl)+(f3_x@2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['174.138.3.166', 'smalltasksonline.works']
 
