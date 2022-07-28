c105f8d9df19311753c6c41c4ae194d16dbf59bd
Joel <nyambok97@gmail.com>
2022-07-28 13:55:40 +0300

    SimpleAnalytic

--- smalltasks/settings.py
+++ smalltasks/settings.py
@@ -39,11 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
+    'django.contrib.sessions'
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tasks.apps.TasksConfig',
     'django_filters',
+     'simpleanalytics',
 ]
 
 MIDDLEWARE = [
